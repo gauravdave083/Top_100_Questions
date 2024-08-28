c19196dfb9ea409ca2da6e87deaed6e59e5c0f52
Gaurav Vijay Dave <122374158+gauravdave083@users.noreply.github.com>
2024-08-28 11:05:33 +0000

    Question 9

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -36,3 +36,17 @@ else:
     print("prime")
     
 # Method 3: Basic Recursion 
+
+num =15
+def checkPrime(num, iter=2):
+    if num == iter:
+        return True
+    if num%iter == 0:
+        return False
+    if num<2:
+        return False
+    return checkPrime(num, iter+1)
+if checkPrime(num)==True:
+    print("prime")
+else:
+    print("Not Prime")
