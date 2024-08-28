0c360cef273fda75a5b6a7907258ca24b05104ff
Gaurav Vijay Dave <122374158+gauravdave083@users.noreply.github.com>
2024-08-28 08:58:55 +0000

    question 6

---
+++ Basics/GreatestOfTwoNumbers.py
@@ -0,0 +1,18 @@
+# Find the Greatest of the Two Numbers
+# Given two integer inputs as number1 and number2, the objective is to find the largest among the two. Therefore we’ll   write a code to Find the Greatest of the Two Numbers in Python Language.
+
+# Example
+# Input : 20 40
+# Output : 40
+
+# We will use 3 methods to solve the above question
+
+# Method 1: Using if-else Statements
+# Method 2: Using Ternary Operator
+# Method 3: Using inbuilt max() Function
+
+num1, num2 = 20, 30
+if num1>num2:
+    print(num1)
+else:
+    print(num2)
