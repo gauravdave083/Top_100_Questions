916182449c858c22a558b4fa9bcb9875d94bf71c
Gaurav Vijay Dave <122374158+gauravdave083@users.noreply.github.com>
2024-08-28 11:23:07 +0000

    Fibonnachi

---
+++ Recursion/FibonacciSeries.py
@@ -0,0 +1,16 @@
+# f(o) = 0
+# f(1) = 1
+# f(2) = f(1) + f(0)
+# f(n) = f(n-1) + f(n-2)
+
+def Fibonacci(n):
+    if n < 0:
+        print("incorrect input")
+    elif n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+    
+print(Fibonacci(9))
