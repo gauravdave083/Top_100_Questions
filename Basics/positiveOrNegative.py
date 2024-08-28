78375c80cde35a187d134b625b846180f01a04f1
Gaurav Vijay Dave <122374158+gauravdave083@users.noreply.github.com>
2024-08-28 07:52:42 +0000

    code

---
+++ Basics/positiveOrNegative.py
@@ -0,0 +1,14 @@
+# Check if a Number is Positive and Negative in Python
+# Given an integer input, The objective is to write a code to Check if a Number is Positive or Negative in Python Language.
+
+# For Instance,
+# Input : num = 20
+# Output : The number is Positive.
+
+num = 15
+if num > 0:
+    print('Positive')
+elif num < 0:
+    print('Negative')
+else:
+    print('Zero')
