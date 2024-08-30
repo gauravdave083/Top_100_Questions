c1d2549dacd273e5a850937ea9325d423c54731d
gauravdave083 <gd836398@gmail.com>
2024-08-30 13:06:56 +0530

    practice

--- Strings/countNumberOfVowels.py
+++ Strings/countNumberOfVowels.py
@@ -2,5 +2,13 @@ string = 'ALKJEtgdb'
 
 count = 0
 
+string = string.lower()
+
 for i in string:
-    
+    if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
+        count +=1
+
+if count == 0:
+    print("No vowel founds")
+else:
+    print("Total vowels are: " + str(count))
