06390c5c2b4b80cfcf26d7eff517d31b83c9d13e
gauravdave083 <gd836398@gmail.com>
2024-08-30 13:29:09 +0530

    practice

--- Strings/countNumberOfVowels.py
+++ Strings/countNumberOfVowels.py
@@ -1,3 +1,8 @@
+# Count the number of vowels in a string
+# Program to Count the Number of vowels we’re going to check how many vowels are present in a given String . There are five vowels in English vocabulary, they are – ‘a’, ‘e’, ‘i’, ‘o’, ‘u’.
+
+# For Example, in the string prepinsta then in that case then vowesl are 3 (a,e,i)
+
 string = 'ALKJEtgdb'
 
 count = 0
