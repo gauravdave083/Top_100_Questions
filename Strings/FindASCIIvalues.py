9fb505b6c58d7814285cfb9bb3196475ee7d2747
gauravdave083 <gd836398@gmail.com>
2024-08-30 10:16:56 +0530

    practice

---
+++ Strings/FindASCIIvalues.py
@@ -0,0 +1,8 @@
+# ASCII value of  character in Python
+# Here, On this page we will discuss program to find the ASCII value of a character in Python. The machine doesn’t have the capacity to understand languages that humans do, the machine understands binary language and converts everything to binary format.
+
+Char = 'z'
+
+Ascii_val = ord(Char)
+
+print('The ASCII vlaue of', Char, 'is', Ascii_val)
