90e846dd34fa0c1954ca54bedf8a84236bf5dba5
gauravdave083 <gd836398@gmail.com>
2024-08-30 18:34:08 +0530

    practice

--- Strings/RemoveVowelsFromString.py
+++ Strings/RemoveVowelsFromString.py
@@ -2,3 +2,4 @@
 
 # The string is built with 2 different types of alphabets grouped as vowels and consonants. In this python program, we will be searching for each alphabet that belongs to the group of vowels and will delete it and print the strings with remaining consonants. As we know String is un-mutable in python so we will initialize a new string and concatenate alphabets that don’t belong to the group of vowels and print it.
 
+
