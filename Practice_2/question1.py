237f45c8fbdd27c77754edcf3ad367cb40a1efe5
gauravdave083 <gd836398@gmail.com>
2024-08-31 17:44:30 +0530

    practice

---
+++ Practice_2/question1.py
@@ -0,0 +1,8 @@
+num = int(input("enter  the  number: "))
+
+if num > 0:
+    print("The number", num, "is positive")
+elif num == 0:
+    print("It is Zero")
+else:
+    print("The number", num, "is negative")
