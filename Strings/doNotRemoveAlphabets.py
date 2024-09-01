c80fc07380ad4a23dbd9e978b911fa0d06c43f01
gauravdave083 <gd836398@gmail.com>
2024-09-01 15:48:40 +0530

    practice

--- Strings/doNotRemoveAlphabets.py
+++ Strings/doNotRemoveAlphabets.py
@@ -1,3 +1,11 @@
+# Remove all character from a string except alphabets
+# In this article we will learn about how to Remove all character from a string except alphabets
+
+# We will do this by checking ASCII values of each character present in the string and remove all character that does not lie in the range of alphabetic ASCII value whether it is an uppercase letter or a lowercase letter.
+
+
+
+
 String1 = "#Justice!For@Chutki123"
 
 String2 = ''
