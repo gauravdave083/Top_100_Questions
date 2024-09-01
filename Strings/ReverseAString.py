eeaffc129f54d03457ae14f3173500a791845538
gauravdave083 <gd836398@gmail.com>
2024-09-01 15:45:04 +0530

    practice

---
+++ Strings/ReverseAString.py
@@ -0,0 +1,11 @@
+# What are strings and how to reverse a string?
+
+# The string is an immutable sequence data type. It is a sequence of Unicode characters wrapped inside a single, double or triple quote. The task is to write a program to reverse the given string.
+# Example :
+
+# Input : Hello world
+# Output: dlrow olleH
+
+#using slicing:
+string = "Hello world"
+print(string[::-1])
