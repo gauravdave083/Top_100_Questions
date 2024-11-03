49b6580ca228eff4da18c87e62a8dd3b1cd41df3
gauravdave083 <gd836398@gmail.com>
2024-11-03 18:11:56 +0530

    practice

---
+++ Practice_2/PrimeNumberInRange.py
@@ -0,0 +1,7 @@
+# Find the Prime Numbers in a Given Range in Python
+# Given two integer as Limits, low and high, the objective is to write a code to in Python Find Prime Numbers in a Given Range in Python Language. To do so we’ll use     nested loops to check for the Prime while Iterating through the range.
+# Example
+# Input : low = 2 , high = 10
+# Output : 2 3 5 7
+
+
